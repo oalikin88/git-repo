a709633ffe1241a1701e630130068cdc0608a8c7
oalikin88 <67868065+oalikin88@users.noreply.github.com>
2021-10-18 23:13:50 +0300

    Добавлен класс Test2

---
+++ MyProject/src/test2/Test2.java
@@ -0,0 +1,15 @@
+package test2;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		int b = 7;
+		int c = 3;
+		String s1 = "Hello";
+		String s2 = "World!";
+		System.out.println(s1 + " " + s2);
+	}
+
+}
