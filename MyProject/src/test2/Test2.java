d351c31baacc74633a2a3b01d603caad04f3d96c
oalikin88 <67868065+oalikin88@users.noreply.github.com>
2021-10-19 00:41:22 +0300

    Добавил восклицательные знаки

--- MyProject/src/test2/Test2.java
+++ MyProject/src/test2/Test2.java
@@ -7,8 +7,8 @@ public class Test2 {
 		int a = 5;
 		int b = 7;
 		int c = 3;
-		String s1 = "Hello!";
-		String s2 = "World!";
+		String s1 = "Hello!!";
+		String s2 = "World!!";
 		System.out.println(s1 + " " + s2);
 		System.out.println(a + b);
 		System.out.println(a * b * c);
--- MyProject/src/test3/Test3.java
+++ MyProject/src/test3/Test3.java
@@ -11,7 +11,7 @@ public class Test3 {
 		Test3 test = new Test3();
 		System.out.println(test.age);
 		System.out.println(test.height);
-		System.out.println("Hello world!");
+		System.out.println("Hello world!!!");
 
 
 	}
