0db4b40cfe81378a1b763d1a1e1d247dd15bee00
oalikin88 <67868065+oalikin88@users.noreply.github.com>
2021-10-19 00:43:13 +0300

    Добавлен код

--- MyProject/src/test3/Test3.java
+++ MyProject/src/test3/Test3.java
@@ -10,7 +10,7 @@ public class Test3 {
 		System.out.println("Java!");
 		Test3 test = new Test3();
 		System.out.println(test.age);
-		System.out.println(test.height);
+		System.out.println(test.height + " Hello world!");
 		System.out.println("Hello world!!!");
 
 
