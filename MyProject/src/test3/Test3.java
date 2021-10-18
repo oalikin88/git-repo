febeade0ebc87d3c5696c8462270632f4c949389
oalikin88 <67868065+oalikin88@users.noreply.github.com>
2021-10-19 00:37:29 +0300

    Добавлен код

--- MyProject/src/test3/Test3.java
+++ MyProject/src/test3/Test3.java
@@ -4,10 +4,11 @@ public class Test3 {
 	String name = "Oleg";
 	int age = 33;
 	int height = 192;
+	int weight = 84;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Java!");
+		System.out.println("Java!!!!");
 		Test3 test = new Test3();
 		System.out.println(test.age);
 		System.out.println(test.height);
